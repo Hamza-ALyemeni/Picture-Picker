931f79fc02c627f1f59300fc4b65178d63d77a18
Your Name <you@example.com>
2024-07-04 17:03:49 +0300

    useEffect solving another problem

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
 const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place)=>place.id === id));
 
 function App() {
-  const modal = useRef();
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const selectedPlace = useRef();
   const [availablePlaces,setAvailablePlaces] = useState([]);
@@ -31,12 +30,12 @@ function App() {
   
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -62,8 +61,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
-
+    setModalIsOpen(false);
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem(
       'selectedPlaces', 
@@ -73,8 +71,8 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen}>
-        <DeleteConfirmation
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
         />
